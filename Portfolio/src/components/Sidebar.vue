7209d74dd846918b261d63a238f6a583ffe9f558
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2022-11-01 17:15:29 +0200

    Delete Portfolio directory

--- Portfolio/src/components/Sidebar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div class="sidebar">
-  <img class="mx-auto my-2" src="@/assets/logo.svg" width="150" />
-  <div class="text-white text-center text-2xl">Ilias Palaigeorgiou</div>
-  <div class="text-white mx-auto btn">Software Engineer</div>
-
-</div>
-</template>
