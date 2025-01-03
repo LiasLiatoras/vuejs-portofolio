33cd38a0f5df0f8c70b1fc46d91dac21d370dcd1
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2025-01-03 03:17:28 +0200

    Add Deploy.yml

--- portfolio/src/views/AboutView.vue
+++ portfolio/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 1</h1>
     <img alt="Vue logo" src="../assets/profile-photo.png">
   </div>
 </template>
