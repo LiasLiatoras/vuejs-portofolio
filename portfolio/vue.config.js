d5ca552ea40c15db947d7df8258c9a191fa6860e
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2025-01-03 03:28:45 +0200

    Update vue.config.js

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vuejs-portofolio/',
+  publicPath:  process.env.NODE_ENV === 'production'
+  ? '/vuejs-portofolio/'
+  : '/',
   transpileDependencies: true
 })
