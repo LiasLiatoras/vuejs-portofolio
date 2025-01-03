fb88ff3555b1503b67923c2bbc5c38c232207dc2
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2025-01-03 03:21:18 +0200

    Update AboutView.vue

--- portfolio/src/views/AboutView.vue
+++ portfolio/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page 1</h1>
-    <img alt="Vue logo" src="../assets/profile-photo.png">
+    <img alt="Vue logo test" src="../assets/profile-photo.png">
   </div>
 </template>
