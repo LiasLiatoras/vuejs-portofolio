7cfdb301810f95148ff2412d8231f32450c87f98
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2025-04-19 02:19:56 +0300

    fix router

--- portfolio/src/router/index.ts
+++ portfolio/src/router/index.ts
@@ -18,7 +18,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory('/vuejs-portofolio/'),
+  history: createWebHashHistory(),
   routes
 })
 
