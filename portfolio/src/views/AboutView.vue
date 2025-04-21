ef91027d26378ad199b411b2682d8d4d10868d76
Ilias Palaigeorgiou <iliaspalaig@gmail.com>
2025-04-21 18:10:35 +0300

    Change About page

--- portfolio/src/views/AboutView.vue
+++ portfolio/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page test no1</h1>
+    <h1>This is an about page</h1>
     <img alt="Vue logo test" src="../assets/profile-photo.png">
   </div>
 </template>
